a6727cc1f17b047524e68fbcd212f3fd4b2f552d
dalessandrosantos <dalessandrosantos83@gmail.com>
2024-10-01 17:37:45 -0300

    finalizado

--- script.js
+++ script.js
@@ -4,13 +4,31 @@ const taskInput = document.getElementById("taskInput");
 function addTask() {
     const taskText = taskInput.value.trim();
     if (taskText !== "") {
+
+        const maxText = taskText.substring(0, 35) 
+
         const li = document.createElement("li");
         li.innerHTML = `
-        <span>${taskText}</span>
+        <span>${maxText}</span>
         <button class="editButton" onClick="editTask(this)">Editar</button>
         <button class="deleteButton" onClick="deleteTask(this)">Remover</button>
         `;
         taskList.appendChild(li);
         taskInput.value = "";
     }
+}
+
+function editTask(button) {
+    const li = button.parentElement;
+    const span = li.querySelector("span");
+    const newText = prompt("Editar tarefa:", span.textContent);
+    if (newText !== null && newText.trim() !== "") {
+        span.textContent = newText.trim();
+    }
+}
+
+function deleteTask(button) {
+    const li = button.parentElement;
+
+taskList.removeChild(li); 
 }
